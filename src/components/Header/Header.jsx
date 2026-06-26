ee2c20f16d7bb1279e13d192acfeed8565a1a4a8
Needleless1745 <josemsanchez8@gmail.com>
2026-06-26 03:13:42 -0500

    Hero/search area further styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import logo from "../../assets/logo.svg";
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="" className="header__logo" />
-      <div className="header__right__side">
+      <img src={logo} alt="NewsExplorer logo" className="header__logo" />
+      <div className="header__right-side">
         <button className="home__btn">Home</button>
         <button className="sign__in-btn">Sign in</button>
       </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,12 @@
 import "./Hero.css";
 import SearchForm from "../SearchForm/SearchForm";
-// main__search == msearch
 
 function Hero() {
   return (
-    <section className="main__search">
-      <div className="search__content">
-        <h1 className="ms__title">What's going on in the world?</h1>
-        <p className="ms__subtitle">
+    <section className="hero">
+      <div className="hero__content">
+        <h1 className="hero__title">What's going on in the world?</h1>
+        <p className="hero__subtitle">
           Find the latest news on any topic and save them in your personal
           account.
         </p>
