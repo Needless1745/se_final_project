3ad77fa123c8470b5500cc91b091adc90cfa832a
Needleless1745 <josemsanchez8@gmail.com>
2026-06-28 01:30:39 -0500

    footer init markup&styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,47 @@
+import "./Footer.css";
+import githubIcon from "../../assets/github.svg";
+import linkedinIcon from "../../assets/Linkedin.svg";
+
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer__copyright">
-        <p className="copyright__text">&copy</p>
+    <footer className="footer">
+      <div className="footer__content">
+        <div className="footer__copyright">
+          <p className="copyright__text">
+            &copy; 2026 SuperSite, Powered by News API
+          </p>
+        </div>
+        <div className="footer__right-side">
+          <nav className="footer__nav-links">
+            <a href="/" className="footer__link">
+              Home
+            </a>
+            <a href="https://tripleten.com" className="footer__link">
+              TripleTen
+            </a>
+          </nav>
+          <nav className="footer__icons">
+            <a href="https://github.com/Needless1745" className="footer__icon">
+              <img
+                src={githubIcon}
+                alt="github icon"
+                className="github__icon social__icon"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jose-s-9795bb123/"
+              className="footer__icon"
+            >
+              <img
+                src={linkedinIcon}
+                alt="Linkedin icon"
+                className="linkedin__icon social__icon"
+              />
+            </a>
+          </nav>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
