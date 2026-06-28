436e0a7c7fc2c9c6bac2728e7f9983a965dbed40
Needleless1745 <josemsanchez8@gmail.com>
2026-06-28 00:38:02 -0500

    About init styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import Main from "../Main/Main";
-import Header from "../Header/Header";
 import About from "../About/About";
 import Footer from "../Footer/Footer";
 import SavedNews from "../SavedNews/SavedNews";
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
       <div className="hero__content">
         <Header />
         <div className="hero__body">
-          <h1 className="hero__title">What's going on in the world?</h1>
+          <h1 className="hero__title">What&apos;s going on in the world?</h1>
           <p className="hero__subtitle">
             Find the latest news on any topic and save them in your personal
             account.
