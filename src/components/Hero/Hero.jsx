20fa46b05d3d836c5dc5489140858533ce86a0bf
Needleless1745 <josemsanchez8@gmail.com>
2026-06-30 20:30:23 -0500

    Further css revisions 'Header/Hero.css'

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,15 +7,14 @@ function Hero() {
     <section className="hero">
       <div className="hero__content">
         <Header />
-        <div className="hero__body">
-          <h1 className="hero__title">What&apos;s going on in the world?</h1>
-          <p className="hero__subtitle">
-            Find the latest news on any topic and save them in your personal
-            account.
-          </p>
 
-          <SearchForm />
-        </div>
+        <h1 className="hero__title">What&apos;s going on in the world?</h1>
+        <p className="hero__subtitle">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+
+        <SearchForm />
       </div>
     </section>
   );
